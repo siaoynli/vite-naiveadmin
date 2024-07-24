8712326c4fd93fc31de0d6ea3bc839e8b145a435
siaoynli <siaoynli@126.com>
2024-07-24 10:04:49 +0800

    ok

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bg-blue-500 text-white p-4 rounded-lg">
+    <h1 class="text-2xl font-bold">Hello, Tailwind!</h1>
+  </div>
+  <n-button>Naive UI Button</n-button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
